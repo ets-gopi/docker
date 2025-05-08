f73046d4fa197f132477e35f6f49da2090044ae7
ets-gopi <gopi.m@entitledarts.com>
2025-05-08 23:31:00 +0530

    added restarts polices

--- visits-starter/index.js
+++ visits-starter/index.js
@@ -1,17 +1,24 @@
 import express from "express";
 import { createClient } from "redis";
+import process from "process";
 
 const app = express();
 const client = await createClient({ url: "redis://redis-server:6379" })
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect();
+
 await client.set("visits", 0);
 
 app.get("/", async (req, res) => {
+  //process.exit(1);
   const value = await client.get("visits");
   const updated = Number(value) + 1;
   await client.set("visits", updated);
   res.send(`views ${updated}`);
+});
+
+app.get("/health-check", (req, res) => {
+  res.send("i am testing");
 });
 
 app.listen(8081, () => {
